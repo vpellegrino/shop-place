a816c88e6871c0d7c8bb0cd4ea8a868efbab801e
vpellegr <valentino_pellegrino@bmc.com>
2019-11-16 12:46:01 +0100

    updating swagger config

--- src/main/java/com/acme/shop/config/SwaggerConfig.java
+++ src/main/java/com/acme/shop/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.acme.shop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -20,21 +21,23 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.acme.shop"))
-                .paths(regex("/shop-place/.*"))
+                .paths(regex("/api.*"))
                 .build()
-                .apiInfo(buildApiInfo());
+                .apiInfo(buildApiInfo())
+                .useDefaultResponseMessages(false);
     }
 
     private ApiInfo buildApiInfo() {
-        return new ApiInfo(
-                "Shop Place Application",
-                "REST/JSON web service in Java to manage products and orders",
-                "1.0",
-                "Terms of Service",
-                new Contact("Valentino Pellegrino", "https://www.linkedin.com/in/valentinopellegrino/",
-                        "pellegrino.valentino@gmail.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html");
+        return new ApiInfoBuilder()
+                .title("Shop Place Application")
+                .description("REST/JSON web service in Java to manage products and orders")
+                .version("1.0")
+                .termsOfServiceUrl("Terms of Service")
+                .contact(new Contact("Valentino Pellegrino", "https://www.linkedin.com/in/valentinopellegrino/",
+                        "pellegrino.valentino@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licesen.html")
+                .build();
     }
 
 }

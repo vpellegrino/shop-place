f68f8fbd2f6308e9a453ac00a4a42d4f587b735b
vpellegr <valentino_pellegrino@bmc.com>
2019-11-17 09:57:22 +0100

    creating DB init DDLs

---
+++ db/db.sql
@@ -0,0 +1,37 @@
+CREATE TABLE public.product
+(
+    product_id serial NOT NULL,
+    product_name text NOT NULL,
+    unit_price numeric NOT NULL,
+    CONSTRAINT product_pkey PRIMARY KEY (product_id)
+);
+
+CREATE TABLE public.order
+(
+    order_id serial NOT NULL,
+    buyer_email text NOT NULL,
+    order_date date NOT NULL,
+    CONSTRAINT order_pkey PRIMARY KEY (order_id)
+);
+
+CREATE TABLE public.order_detail
+(
+    order_id integer NOT NULL,
+    product_id integer NOT NULL,
+    product_quantity numeric NOT NULL,
+    unit_price numeric NOT NULL
+);
+
+ALTER TABLE public.order_detail
+ADD CONSTRAINT pk_order_detail
+PRIMARY KEY(order_id, product_id);
+
+ALTER TABLE public.order_detail
+ADD CONSTRAINT fk_detail_order
+FOREIGN KEY (order_id) REFERENCES
+public.order (order_id);
+
+ALTER TABLE public.order_detail
+ADD CONSTRAINT fk_detail_product
+FOREIGN KEY (product_id) REFERENCES
+public.product (product_id);

9f816e9a4c8224cf751c08981f544f0741db480f
vpellegr <valentino_pellegrino@bmc.com>
2019-11-18 19:37:03 +0100

    calling services in related controllers

--- src/main/java/com/acme/shop/controller/OrderController.java
+++ src/main/java/com/acme/shop/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.acme.shop.controller;
 
 import com.acme.shop.dto.ApiError;
-import com.acme.shop.dto.Order;
+import com.acme.shop.dto.OrderDTO;
+import com.acme.shop.service.OrderService;
 import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,9 @@ public class OrderController {
 
     private static final String ACCEPTED_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
+    @Autowired
+    private OrderService orderService;
+
     @PostMapping(path = "orders", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Places an order", nickname = "placeOrder", response = BigDecimal.class)
     @ApiResponses(value = {
@@ -28,25 +33,25 @@ public class OrderController {
             @ApiResponse(code = 400, message = "Invalid input", response = ApiError.class),
             @ApiResponse(code = 500, message = "An unexpected error occurred", response = ApiError.class)})
     @ResponseStatus(HttpStatus.CREATED)
-    public BigDecimal placeOrder(@RequestBody Order order) {
-        return null;
+    public Long placeOrder(@RequestBody OrderDTO order) {
+        return orderService.createOrder(order);
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     @GetMapping(path = "orders", produces = APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Retrieves the list of all orders within a given time period, specified by two (inclusive) dates",
-            nickname = "getOrderListBetweenDates", response = Order.class, responseContainer = "List")
+            nickname = "getOrderListBetweenDates", response = OrderDTO.class, responseContainer = "List")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "The list of all orders within a given time period", response = Order.class, responseContainer = "List"),
+            @ApiResponse(code = 200, message = "The list of all orders within a given time period", response = OrderDTO.class, responseContainer = "List"),
             @ApiResponse(code = 400, message = "End date must not be before the start date", response = ApiError.class),
             @ApiResponse(code = 500, message = "An unexpected error occurred", response = ApiError.class)})
-    public List<Order> getOrderListBetweenDates(
+    public List<OrderDTO> getOrderListBetweenDates(
             @ApiParam(value = "Inclusive start date", required = true, example = "2012-07-10 14:58:00")
             @RequestParam(value = "start") @DateTimeFormat(pattern = ACCEPTED_DATETIME_FORMAT) Date startDate,
 
-            @ApiParam(value = "Inclusive end date", example = "2014-07-10 00:00:00")
+            @ApiParam(value = "Inclusive end date, if not specified current time will be considered", example = "2014-07-10 00:00:00")
             @RequestParam(value = "end", required = false) @DateTimeFormat(pattern = ACCEPTED_DATETIME_FORMAT) Optional<Date> endDate) {
-        return null;
+        return orderService.getOrdersInPeriod(startDate, endDate.orElse(new Date()));
     }
 
 }
--- src/main/java/com/acme/shop/controller/ProductController.java
+++ src/main/java/com/acme/shop/controller/ProductController.java
@@ -1,11 +1,10 @@
 package com.acme.shop.controller;
 
 import com.acme.shop.dto.ApiError;
-import com.acme.shop.dto.Product;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import com.acme.shop.dto.ProductDTO;
+import com.acme.shop.service.ProductService;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +18,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Api(value = "product-api", tags = "Products API")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @PostMapping(path = "products", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Creates a new product", nickname = "createProduct", response = BigDecimal.class)
     @ApiResponses(value = {
@@ -26,27 +28,29 @@ public class ProductController {
             @ApiResponse(code = 400, message = "Invalid input", response = ApiError.class),
             @ApiResponse(code = 500, message = "An unexpected error occurred", response = ApiError.class)})
     @ResponseStatus(HttpStatus.CREATED)
-    public BigDecimal createProduct(@RequestBody Product product) {
-        return null;
+    @ApiModelProperty(value = "product_id", hidden = true)
+    public Long createProduct(@RequestBody ProductDTO product) {
+        return productService.createProduct(product);
     }
 
     @GetMapping(path = "products", produces = APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Retrieves the list of all products", nickname = "getProductList", response = Product.class, responseContainer = "List")
+    @ApiOperation(value = "Retrieves the list of all products", nickname = "getProductList", response = ProductDTO.class, responseContainer = "List")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "The list of all product", response = Product.class, responseContainer = "List"),
+            @ApiResponse(code = 200, message = "The list of all product", response = ProductDTO.class, responseContainer = "List"),
             @ApiResponse(code = 500, message = "An unexpected error occurred", response = ApiError.class)})
-    public List<Product> getProductList() {
-        return null;
+    public List<ProductDTO> getProductList() {
+        return productService.getProductList();
     }
 
     @PutMapping(path = "products/{productId}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Fully updates the specified product", nickname = "updateProduct")
     @ApiResponses(value = {
             @ApiResponse(code = 204, message = "The specific product has been correctly updated"),
-            @ApiResponse(code = 404, message = "The specified product was not found", response = ApiError.class),
+            @ApiResponse(code = 404, message = "The specific product was not found", response = ApiError.class),
             @ApiResponse(code = 500, message = "An unexpected error occurred", response = ApiError.class)})
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateProduct(@PathVariable("productId") BigDecimal productId, @RequestBody Product product) {
+    public void updateProduct(@PathVariable("productId") Long productId, @RequestBody ProductDTO product) {
+        productService.updateProduct(productId, product);
     }
 
 }
